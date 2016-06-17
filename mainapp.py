50e8b5b309c42572b72f92a7cc33ff4cfe1b03b3
Richard Schwalk <richard.schwalk@gmail.com>
2016-06-17 19:33:02 +0200

    Renamed some files

--- mainapp.py
+++ mainapp.py
@@ -1,11 +1,52 @@
-from flask import Flask
+import json
+import datetime
+import flask
+import httplib2
 
-app = Flask(__name__)
+from apiclient import discovery
+from oauth2client import client
+
+app = flask.Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    if 'credentials' not in flask.session:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    credentials = client.OAuth2Credentials.from_json(flask.session['credentials'])
+    if credentials.access_token_expired:
+        return flask.redirect(flask.url_for('oauth2callback'))
+    else:
+        http_auth = credentials.authorize(httplib2.Http())
+        service = discovery.build('calendar', 'v3', http=http_auth)
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        eventsResult = service.events().list(
+            calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+            orderBy='startTime').execute()
+        events = eventsResult.get('items', [])
+        return json.dumps(events)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    flow = client.flow_from_clientsecrets(
+        'client_secret.json',
+        scope='https://www.googleapis.com/auth/calendar.readonly',
+        redirect_uri=flask.url_for('oauth2callback', _external=True))
+    if 'code' not in flask.request.args:
+        auth_uri = flow.step1_get_authorize_url()
+        return flask.redirect(auth_uri)
+    else:
+        auth_code = flask.request.args.get('code')
+        credentials = flow.step2_exchange(auth_code)
+        flask.session['credentials'] = credentials.to_json()
+        return flask.redirect(flask.url_for('index'))
+
 
 if __name__ == '__main__':
+    import uuid
+
+    app.secret_key = str(uuid.uuid4())
+    app.debug = False
     app.run()
